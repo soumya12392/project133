4a162fb7e4a0d023bec92373952697737662321d
Soumya Singh <83289414+soumya12392@users.noreply.github.com>
2022-10-10 20:42:17 +0530

    Add files via upload

---
+++ television.js
@@ -0,0 +1,50 @@
+var img = "";
+var status = "";
+var objectDetector = "";
+var objects = [];
+
+function preload() {
+    img = loadImage("television.jpg");
+}
+
+function setup() {
+    var canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            document.getElementById("detected").innerHTML = "There is 1 big object in this image, out of which the CoCoSSD Model detected " + objects.length + " object.";
+            
+            fill("#ff0000");
+            var percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#ff0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            console.log("Drawn");
+        }
+    }
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+    }
+    objects = results;
+}
